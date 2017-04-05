2adc905c4bfd6c74e3c4c4821aa4d9f508082cc1
Douwe van Gijn <kipstad@hotmail.com>
2017-04-05 17:17:42 +0200

    first spring attempt

--- Assets/Scripts/ClothScript.cs
+++ Assets/Scripts/ClothScript.cs
@@ -8,6 +8,9 @@ public class ClothScript : MonoBehaviour
     public float distance = 1f;
     public int width = 11;
     public int height = 11;
+    public float springFactor = 10f;
+    public float dampingFactor= 10f;
+    public float invmass = 1f;
     public Vector3 initialPos = new Vector3(-5, 12, 0);
     private Vector3[] velocities;
 
@@ -58,17 +61,32 @@ public class ClothScript : MonoBehaviour
         mesh.normals = normals;
     }
 
+    // Linear Strain model (Hooke's Law)
+    Vector3 dvel(Vector3 r, Vector3 v)
+    {
+        float spring = springFactor * (r.magnitude - distance);
+        float damp = dampingFactor * Vector3.Dot(v, r.normalized);
+        Vector3 f = r.normalized * (spring + damp);
+        Vector3 a = f * invmass;
+        Vector3 vel = a * Time.deltaTime;
+        Debug.LogFormat("spring:{0} damp:{1} f:{2} a:{3} v:{4}", spring, damp, f, a, v);
+        return vel;
+    }
+ 
     void Update()
     {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         Vector3[] vertices = mesh.vertices;
         int[] triangles = mesh.triangles;
         //if changing mesh.triangles (when tearing) call mesh.Clear() first
-        
+
         //gravity
         for (int i = 0; i < velocities.Length; i++)
         {
-            velocities[i].y -= 9.81f * Time.deltaTime;
+            if (i != 0 && i != (width - 1))
+            {
+                velocities[i].y -= 9.81f * Time.deltaTime;
+            }
         }
 
         //apply velocities
@@ -79,9 +97,27 @@ public class ClothScript : MonoBehaviour
 
         //collide with floor (and sphere later)
 
-
         //solve springs
-
+        for (int y = 0; y < width-1; y++)
+        {
+            for (int x = 0; x < height-1; x++)
+            {
+                int w = width - 1;
+                int h = height - 1;
+                if (x == 0 && y == 0) continue;
+                if (x == w && y == 0) continue;
+                int i = y * width + x;
+                if (x > 0) velocities[i] += dvel(vertices[i-1]   - vertices[i], 
+                                                 velocities[i-1] - velocities[i]);
+                if (x < w) velocities[i] += dvel(vertices[i+1]   - vertices[i],
+                                                 velocities[i+1] - velocities[i]);
+                if (y > 0) velocities[i] += dvel(vertices[i-width]   - vertices[i], 
+                                                 velocities[i-width] - velocities[i]);
+                if (y < h) velocities[i] += dvel(vertices[i+width]   - vertices[i], 
+                                                 velocities[i+width] - velocities[i]);
+            }
+        }
+        
 
         mesh.vertices = vertices;
         mesh.RecalculateBounds();
