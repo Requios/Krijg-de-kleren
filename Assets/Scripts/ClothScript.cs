3da8fb39bfe43db1bbb5587cff5306e6aa3ab1ff
Christian Knaapen <christianknaapen@hotmail.com>
2017-04-12 17:42:04 +0200

    100x100 te slap

--- Assets/Scripts/ClothScript.cs
+++ Assets/Scripts/ClothScript.cs
@@ -44,14 +44,14 @@ public class ClothScript : MonoBehaviour
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < (width - 1); x++)
             {
-                springs[idx++] = new Spring(y * width + x, y * width + x + 1, 1);
+                springs[idx++] = new Spring(y * width + x, y * width + x + 1, distance);
             }
         }
         // vertical springs
         for (int y = 0; y < (height-1); y++) {
             for (int x = 0; x < width; x++)
             {
-                springs[idx++] = new Spring(y * width + x, (y+1) * width + x, 1);
+                springs[idx++] = new Spring(y * width + x, (y+1) * width + x, distance);
             }
         }
         // diagonal springs
@@ -60,9 +60,9 @@ public class ClothScript : MonoBehaviour
             for (int x = 0; x < (width-1); x++)
             {
                 // top left to bottom right
-                springs[idx++] = new Spring(y * width + x, (y+1) * width + x+1, Mathf.Sqrt(2)); 
+                springs[idx++] = new Spring(y * width + x, (y+1) * width + x+1, Mathf.Sqrt(2) * distance); 
                 // bottom left to top right
-                springs[idx++] = new Spring((y+1) * width + x, y * width + x+1, Mathf.Sqrt(2));
+                springs[idx++] = new Spring((y+1) * width + x, y * width + x+1, Mathf.Sqrt(2) * distance);
             }
         }
         Debug.Assert(springs.Length == idx);
@@ -79,7 +79,7 @@ public class ClothScript : MonoBehaviour
         {
             for (int j = 0; j < width; j++)
             {
-                vertices[i * width + j] = new Vector3(j, -i, 0) + initialPos;
+                vertices[i * width + j] = new Vector3(j * distance, 0, i * distance) + initialPos;
             }
         }
         mesh.vertices = vertices;
